5cc79bd69858e3175764fb614284084d55534b7e
Bonifácio <jboni2014@gmail.com>
2023-03-15 21:06:34 -0300

    Little Witch
    V0.0.2
    
    Adicionado animações de andar em 4 direções

---
+++ Assets/Scripts/Game/Player/PlayerMovement.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+
+    private Rigidbody2D _rigidbody;
+    Vector2 MoveInput;
+    private Animator animator;
+
+    void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        AnimationPlayer();
+    }
+
+    void FixedUpdate()
+    {
+        _rigidbody.velocity = MoveInput * speed;    
+    }
+
+    void OnMove(InputValue inputValue)
+    {
+        MoveInput = inputValue.Get<Vector2>();
+    }
+
+    void AnimationPlayer()
+    {
+        #region Move
+
+        animator.SetFloat("Horizontal", MoveInput.x);
+        animator.SetFloat("Vertical", MoveInput.y);
+        animator.SetFloat("Speed", MoveInput.sqrMagnitude);
+
+        if(MoveInput.x == 1 || MoveInput.x == -1 || MoveInput.y == 1 || MoveInput.y == -1)
+        {
+            animator.SetFloat("LastHorizontal", MoveInput.x);
+            animator.SetFloat("LastVertical", MoveInput.y);
+        }
+
+        #endregion
+    }
+}
