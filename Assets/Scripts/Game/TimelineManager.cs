f06c33f7d98d105722edf5fbceba2b8367375716
Bonifácio <jboni2014@gmail.com>
2023-07-30 20:10:23 -0300

    Little White
    V1.1.1
    Fiz a primeira cutscene

---
+++ Assets/Scripts/Game/TimelineManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimelineManager : MonoBehaviour
+{
+    private bool fix = false;
+    public Animator playerAnimator;
+    public RuntimeAnimatorController playerAnim;
+    public PlayableDirector director;
+    public GameObject player;
+
+    void OnEnable()
+    {
+        playerAnim = playerAnimator.runtimeAnimatorController;
+        playerAnimator.runtimeAnimatorController = null;
+    }
+
+    void Update()
+    {
+        if(director.state != PlayState.Playing && !fix) 
+        { 
+            fix = true;
+            playerAnimator.runtimeAnimatorController = playerAnim;
+            player.GetComponent<PlayerMovement>().enabled = true;
+        }     
+    }
+}
